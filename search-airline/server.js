59b56fe38f9dd07511d36ee3615b121aaec33063
iilie <iilie@axway.com>
2022-10-11 17:59:51 +0300

    INT - search-airline added.

--- search-airline/server.js
+++ search-airline/server.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const {join, resolve} = require('path');
 const {searchAirline, searchFlights} = require('./service');
+const PORT = process.env.PORT || 8080;
 express()
 	.use(express.static(join(resolve(), 'public')))
-	.get('/airlines/:name', async (request, response) => {
-		response.json(await searchAirline(request.params.name));
-	})
-	.get('/flights/:airline', async (request, response) => {
+	.get('/airlines/:name', async (request, response) =>
+		response.json(await searchAirline(request.params.name))
+	)
+	.get('/flights/:airline', async (request, response) =>
 		response.json(await searchFlights(request.params.airline))
-	})
-	.listen(process.env.PORT || 8080);
+	)
+	.listen(PORT, () => console.log(`Server is running on port ${PORT}.`));
