1750807cbcc5a006270483a5c9db2649c00f99f8
iilie <iilie@axway.com>
2022-11-28 02:34:41 +0200

    INT - Adding tasks.

---
+++ tasks/task-async-component/public/tasks.js
@@ -0,0 +1,51 @@
+HTMLElement.prototype.tasks = function(data) {
+	function createItem(task) {
+		const item = document.createElement('li');
+		const link = document.createElement('a');
+		link.innerText = task.description;
+		link.setAttribute('href', 'javascript:void(0)');
+		link.addEventListener('click', event => {
+			fetch(`/tasks/${task.id}`, {method: 'DELETE'})
+				.then(response => {
+					event.target.parentNode.parentNode.removeChild(event.target.parentNode);
+				});
+		});
+		item.appendChild(link);
+		return item;
+	};
+	const title = document.createElement('h1');
+	title.innerText = data.title;
+	this.appendChild(title);
+	var paragraph = document.createElement('p');
+	const list = document.createElement('ul');
+	data.tasks.forEach(task => list.appendChild(createItem(task)));
+	paragraph.appendChild(list);
+	this.appendChild(paragraph);
+	var paragraph = document.createElement('p');
+	const span = document.createElement('span');
+	span.innerText = 'New Task: ';
+	paragraph.appendChild(span);
+	const input = document.createElement('input');
+	input.setAttribute('id', 'task');
+	paragraph.appendChild(input);
+	this.appendChild(paragraph);
+	var paragraph = document.createElement('p');
+	const button = document.createElement('input');
+	button.setAttribute('type', 'button');
+	button.setAttribute('value', 'Add Task');
+	button.addEventListener('click', event => {
+		fetch(`/tasks`,
+			{
+				method: 'POST',
+				headers: { 'Content-Type': 'application/x-www-form-urlencoded'},
+				body: `task=${document.getElementById('task').value}`
+			})
+			.then(response => response.json())
+			.then(task => {
+				list.appendChild(createItem(task));
+				document.getElementById('task').value = '';
+			});
+	});
+	paragraph.appendChild(button);
+	this.appendChild(paragraph);
+}
---
+++ tasks/task-sync-component/public/tasks.js
@@ -0,0 +1,35 @@
+HTMLElement.prototype.tasks = function(data) {
+	const title = document.createElement('h1');
+	title.innerText = data.title;
+	this.appendChild(title);
+	const form = document.createElement('form');
+	form.setAttribute('action', '/add-task');
+	form.setAttribute('method', 'post');
+	var paragraph = document.createElement('p');
+	const list = document.createElement('ul');
+	data.tasks.forEach(task => {
+		const item = document.createElement('li');
+		const link = document.createElement('a');
+		link.innerText = task.description;
+		link.setAttribute('href', `/remove-task/${task.id}`);
+		item.appendChild(link);
+		list.appendChild(item);
+	});
+	paragraph.appendChild(list);
+	form.appendChild(paragraph);
+	var paragraph = document.createElement('p');
+	const span = document.createElement('span');
+	span.innerText = 'New Task: ';
+	paragraph.appendChild(span);
+	const input = document.createElement('input');
+	input.setAttribute('name', 'task');
+	paragraph.appendChild(input);
+	form.appendChild(paragraph);
+	var paragraph = document.createElement('p');
+	const button = document.createElement('input');
+	button.setAttribute('type', 'submit');
+	button.setAttribute('value', 'Add Task');
+	paragraph.appendChild(button);
+	form.appendChild(paragraph);
+	this.appendChild(form);
+}
