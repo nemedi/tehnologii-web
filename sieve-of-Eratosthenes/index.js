024699daf5e9667f6d676ba27708fc821caf7913
Iulian Ilie Nemedi <iilie@axway.com>
2023-09-18 10:27:48 +0300

    Sieve of Eratosthenes

---
+++ sieve-of-Eratosthenes/index.js
@@ -0,0 +1,23 @@
+function* sieveOfEratosthenes(limit = 100) {
+    var numbers = new Array(limit + 1);
+    numbers.fill(0);
+    var index = 2;
+    while (index <= limit) {
+        let number = index;
+        index++;
+        for (let i = index; i < numbers.length; i++) {
+			if (i % number == 0) {
+				numbers[i] = 1;
+			}
+		}
+		while (index < numbers.length && numbers[index] == 1) {
+			index++;
+		}
+		yield number;
+    }
+}
+
+const generator = sieveOfEratosthenes();
+for (let i = 0; i < 10; i++) {
+    console.log(generator.next().value);
+}
