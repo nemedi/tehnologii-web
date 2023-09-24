d5a942307b198084fcb6681cffc90cd338c18c71
Iulian Ilie Nemedi <iilie@axway.com>
2023-09-24 21:32:52 +0300

    INT - search-name refactored

--- search-name/server.js
+++ search-name/server.js
@@ -3,7 +3,7 @@ const searchName = require('./service');
 const {join, resolve} = require('path');
 const PORT = process.env.PORT || 8080;
 express()
-	.use(express.static(join(resolve(), 'public')))
+	.use(express.static(join(resolve(), 'web')))
 	.get('/names/:name', async (request, response) => 
 		response.json(await searchName(request.params.name))
 	)
