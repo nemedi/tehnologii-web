acbb6ac77a7eef29e13057388f06bb40dd9a47a1
Iulian Ilie-Nemedi <ilie.nemedi.iulian@gmail.com>
2023-11-13 01:06:46 +0200

    INT - World Cup

--- world-cup/client/index.js
+++ world-cup/client/index.js
@@ -2,68 +2,6 @@ window.onload = async () => loadBoard();
 
 const views = {};
 
-async function loadBoard() {
-	const view = await getView('board');
-	const model = await getModel('board');
-	document.querySelector('.content').innerHTML = view;
-	const template = document.querySelector('.board').innerHTML;
-	document.querySelector('.board').innerHTML =
-		Object.getOwnPropertyNames(model)
-			.map(group =>
-				template.replaceAll('${group}', group)
-			)
-			.reduce((html, item) => html += item, '');
-	Object.entries(model).forEach(([group, teams]) => {
-		const element = document.querySelector(`.group.${group} .teams`);
-		const template = element.innerHTML;
-		element.innerHTML = teams.map(team => template
-				.replaceAll('${team}', team)
-			)
-			.reduce((html, item) => html += item, '');
-	});
-	Object.entries(model).forEach(([group, teams]) => {
-		document.querySelectorAll(`.group.${group} .teams`)
-	});	
-}
-
-async function loadStandings(group) {
-	const view = await getView('standings');
-	const standings = await getModel(`standings/${group}`);
-	document.querySelector('.content').innerHTML = view.replaceAll('${group}', group);
-	const template = document.querySelector('tbody').innerHTML;
-	document.querySelector('tbody').innerHTML =
-		standings.map((standing, index) =>
-				template
-					.replaceAll('${rank}', index + 1)
-					.replaceAll('${team}', standing.team)
-					.replaceAll('${mp}', standing.mp)
-					.replaceAll('${w}', standing.w)
-					.replaceAll('${d}', standing.d)
-					.replaceAll('${l}', standing.l)
-					.replaceAll('${gf}', standing.gf)
-					.replaceAll('${ga}', standing.ga)
-					.replaceAll('${gd}', standing.gd)
-					.replaceAll('${pts}', standing.pts))
-			.reduce((html, item) => html += item, '');
-}
-
-async function loadMatches(team) {
-	const view = await getView('matches');
-	const model = await getModel(`matches/${team}`);
-	document.querySelector('.content').innerHTML = view;
-	const template = document.querySelector('.match').innerHTML;
-	document.querySelector('.match').innerHTML = model.map(match =>
-			template
-				.replaceAll('${stage}', match.stage)
-				.replaceAll('${date}', match.date)
-				.replaceAll('${homeTeam}', match.homeTeam)
-				.replaceAll('${awayTeam}', match.awayTeam)
-				.replaceAll('${homeScore}', match.homeScore)
-				.replaceAll('${awayScore}', match.awayScore)
-		)
-		.reduce((html, item) => html += item, '');
-}
-
 async function getView(view) {
 	if (!views[view]) {
 		const response = await fetch(`/views/${view}.html`);
@@ -78,3 +16,63 @@ async function getModel(path) {
 	const body = await response.json();
 	return body;
 }
+
+async function loadBoard() {
+	const view = await getView('board');
+	const model = await getModel('board');
+	document.querySelector('.content').innerHTML = view;
+	const template = document.querySelector('.board').innerHTML;
+	document.querySelector('.board').innerHTML =
+		Object.getOwnPropertyNames(model)
+			.map(item =>
+				template.replaceAll('${group}', item)
+			)
+			.reduce((html, item) => html += item, '');
+	Object.entries(model).forEach(([group, teams]) => {
+		const element = document.querySelector(`.group.${group} .teams`);
+		const template = element.innerHTML;
+		element.innerHTML = teams.map(item => template
+				.replaceAll('${team}', item)
+			)
+			.reduce((html, item) => html += item, '');
+	});
+}
+
+async function loadStandings(group) {
+	const view = await getView('standings');
+	const model = await getModel(`standings/${group}`);
+	document.querySelector('.content').innerHTML = view.replaceAll('${group}', group);
+	const template = document.querySelector('tbody').innerHTML;
+	document.querySelector('tbody').innerHTML =
+		model.map((item, index) =>
+				template
+					.replaceAll('${rank}', index + 1)
+					.replaceAll('${team}', item.team)
+					.replaceAll('${mp}', item.mp)
+					.replaceAll('${w}', item.w)
+					.replaceAll('${d}', item.d)
+					.replaceAll('${l}', item.l)
+					.replaceAll('${gf}', item.gf)
+					.replaceAll('${ga}', item.ga)
+					.replaceAll('${gd}', item.gd)
+					.replaceAll('${pts}', item.pts))
+			.reduce((html, item) => html += item, '');
+}
+
+async function loadMatches(team) {
+	const view = await getView('matches');
+	const model = await getModel(`matches/${team}`);
+	document.querySelector('.content').innerHTML = view;
+	const template = document.querySelector('.match').innerHTML;
+	document.querySelector('.match').innerHTML = model.map(item =>
+			template
+				.replaceAll('${stage}', item.stage)
+				.replaceAll('${date}', item.date)
+				.replaceAll('${homeTeam}', item.homeTeam)
+				.replaceAll('${awayTeam}', item.awayTeam)
+				.replaceAll('${homeScore}', item.homeScore)
+				.replaceAll('${awayScore}', item.awayScore)
+		)
+		.reduce((html, item) => html += item, '');
+}
+
