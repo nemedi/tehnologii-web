3d58664820f9c3157d951231053a90e1613c869a
Iulian Ilie Nemedi <iilie@axway.com>
2023-11-13 12:30:26 +0200

    INT - World Cup

--- world-cup/client/index.js
+++ world-cup/client/index.js
@@ -1,4 +1,20 @@
 window.onload = async () => loadBoard();
+window.addEventListener('render', event => eval(event.call));
+window.addEventListener('hashchange', event => {
+	let index = event.newURL.indexOf('#');
+	if (index > -1) {
+		event.preventDefault();
+		let call = event.newURL.substring(index + 1);
+		let render = new Event('render');
+		render.call = call;
+		window.dispatchEvent(render);
+	}
+});
+
+String.prototype.replaceVariables = function(context) {
+	return Object.entries(context).reduce((result, [key, value]) =>
+		result.replaceAll('${' + key + '}', decodeURI(value)), this);
+}
 
 const views = {};
 
@@ -24,15 +40,15 @@ async function loadBoard() {
 	const template = document.querySelector('.board').innerHTML;
 	document.querySelector('.board').innerHTML =
 		Object.keys(model)
-			.map(item =>
-				template.replaceAll('${group}', item)
+			.map(group =>
+				template.replaceVariables({group})
 			)
 			.reduce((html, item) => html += item, '');
 	Object.entries(model).forEach(([group, teams]) => {
 		const element = document.querySelector(`.group.${group} .teams`);
 		const template = element.innerHTML;
-		element.innerHTML = teams.map(item => template
-				.replaceAll('${team}', item)
+		element.innerHTML = teams.map(team =>
+				template.replaceVariables({team})
 			)
 			.reduce((html, item) => html += item, '');
 	});
@@ -44,34 +60,19 @@ async function loadStandings(group) {
 	document.querySelector('.content').innerHTML = view.replaceAll('${group}', group);
 	const template = document.querySelector('tbody').innerHTML;
 	document.querySelector('tbody').innerHTML =
-		model.map((item, index) =>
-				template
-					.replaceAll('${rank}', index + 1)
-					.replaceAll('${team}', item.team)
-					.replaceAll('${mp}', item.mp)
-					.replaceAll('${w}', item.w)
-					.replaceAll('${d}', item.d)
-					.replaceAll('${l}', item.l)
-					.replaceAll('${gf}', item.gf)
-					.replaceAll('${ga}', item.ga)
-					.replaceAll('${gd}', item.gd)
-					.replaceAll('${pts}', item.pts))
+		model.map((standing, index) => 
+				template.replaceVariables({rank: index + 1,...standing})
+			)
 			.reduce((html, item) => html += item, '');
 }
 
 async function loadMatches(team) {
 	const view = await getView('matches');
 	const model = await getModel(`matches/${team}`);
-	document.querySelector('.content').innerHTML = view;
+	document.querySelector('.content').innerHTML = view.replaceVariables({team});
 	const template = document.querySelector('.match').innerHTML;
-	document.querySelector('.match').innerHTML = model.map(item =>
-			template
-				.replaceAll('${stage}', item.stage)
-				.replaceAll('${date}', item.date)
-				.replaceAll('${homeTeam}', item.homeTeam)
-				.replaceAll('${awayTeam}', item.awayTeam)
-				.replaceAll('${homeScore}', item.homeScore)
-				.replaceAll('${awayScore}', item.awayScore)
+	document.querySelector('.match').innerHTML = model.map(match =>
+			template.replaceVariables(match)
 		)
 		.reduce((html, item) => html += item, '');
 }
--- world-cup/server/service.js
+++ world-cup/server/service.js
@@ -7,13 +7,13 @@ const {readFileSync} = require('fs');
 module.exports = function(path) {
     const data = JSON.parse(readFileSync(path));
     return {
-        getBoard: function() {
+        getBoard() {
             return data.groups;
         },
-        getStandings: function(group) {
+        getStandings(group) {
             return data.standings[group];
         },
-        getMatches: function(team) {
+        getMatches(team) {
             return data.matches.filter(match => match.homeTeam === team || match.awayTeam === team);
         }
     }
