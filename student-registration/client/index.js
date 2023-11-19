0875b8484693dbd5d3b90383116281bbfbe38e68
Iulian Ilie Nemedi <iilie@axway.com>
2023-11-19 15:29:34 +0200

    INT - Student registration

--- student-registration/client/index.js
+++ student-registration/client/index.js
@@ -22,28 +22,10 @@ function memoizer(method) {
 const getView = memoizer(async view =>
 	await (await fetch(`/views/${view}.html`)).text()
 );
-window.onload = () => renderTable();
-async function renderTable() {
-	const view = await getView('table');
-	$('.container').innerHTML = view;
-	$('input[type=button]').onclick = () => goTo('add');
-	const students = await getStudents();
-	if (students.length > 0) {
-		const ids = students.map(student => student.id);
-		const _ = $('tbody').innerHTML;
-		$('tbody').innerHTML = students.map(student => _.render(student)).join('');
-		$('tfoot').innerHTML = '';
-		const editButtons = $$('input[type=button][value=Editeaza]');
-		const deleteButtons = $$('input[type=button][value=Sterge]');
-		ids.forEach((id, index) => {
-			editButtons[index].onclick = () => goTo(`edit/${id}`);
-			deleteButtons[index].onclick = () => goTo(`remove/${id}`);
-		});
-	} else {
-		$('tbody').innerHTML = '';
-	}
+function goTo(path) {
+	window.location.href = `#${path}`;
 }
-
+window.onload = () => goTo('home');
 async function getStudents() {
 	const response = await fetch('/api/students');
 	if (response.status === 200) {
@@ -52,14 +34,24 @@ async function getStudents() {
 		return [];
 	}
 }
-
+function emptyStudent() {
+	return {
+		id: '',
+		firstName: '',
+		lastName: '',
+		bornDate: '',
+		email: '',
+		phone: '',
+		section: '',
+		group: ''
+	};
+}
 async function getStudent(id) {
 	const response = await fetch(`/api/students/${id}`);
 	if (response.status === 200) {
 		return await response.json();
 	}
 }
-
 async function addStudent(student) {
 	const response = await fetch('/api/students', {
 		method: 'POST',
@@ -72,7 +64,6 @@ async function addStudent(student) {
 		goTo('home');
 	}
 }
-
 async function saveStudent(id, student) {
 	const response = await fetch(`/api/students/${id}`, {
 		method: 'PUT',
@@ -85,7 +76,6 @@ async function saveStudent(id, student) {
 		goTo('home');
 	}
 }
-
 async function removeStudent(id) {
 	const response = await fetch(`/api/students/${id}`, {
 		method: 'DELETE'
@@ -94,45 +84,45 @@ async function removeStudent(id) {
 		goTo('home');
 	}
 }
-
+async function renderTable() {
+	const view = await getView('table');
+	$('.container').innerHTML = view;
+	$('input[type=button]').onclick = () => goTo('add');
+	const students = await getStudents();
+	if (students.length > 0) {
+		const _ = $('tbody').innerHTML;
+		$('tbody').innerHTML = students.map(student => _.render(student)).join('');
+		$('tfoot').innerHTML = '';
+		const editButtons = $$('input[type=button][value=Editeaza]');
+		const deleteButtons = $$('input[type=button][value=Sterge]');
+		students.forEach((student, index) => {
+			editButtons[index].onclick = () => goTo(`edit/${student.id}`);
+			deleteButtons[index].onclick = () => goTo(`remove/${student.id}`);
+		});
+	} else {
+		$('tbody').innerHTML = '';
+	}
+}
 async function renderForm(id) {
 	const student = id
 		? await getStudent(id)
-		: {
-			id: '',
-			firstName: '',
-			lastName: '',
-			bornDate: '',
-			email: '',
-			phone: '',
-			section: '',
-			group: ''
-		};
+		: emptyStudent();
 	const view = await getView('form');
 	$('.container').innerHTML = view.render(student);
-	$('form').onsubmit = (event) => processForm(event.target);
+	$('form').onsubmit = event => processForm(event.target);
 	$('input[type=button]').onclick = () => goTo('home');
 }
-
 async function processForm(form) {
-	const id = form['id'].value;
-	const student = {
-		lastName: form['lastName'].value,
-		firstName: form['firstName'].value,
-		bornDate: form['bornDate'].value,
-		email: form['email'].value,
-		phone: form['phone'].value,
-		section: form['section'].value,
-		group: form['group'].value
-	};
+	const student = emptyStudent();
+	Object.keys(student)
+		.forEach(key => student[key] = form[key].value);
+	const id = student.id;
+	delete student.id;
 	if (id.length > 0) {
 		saveStudent(id, student);
 	} else {
 		addStudent(student);
 	}
-}
-function goTo(path) {
-	window.location.href = `#${path}`;
 }
 window.addEventListener('hashchange', event => {
 	let index = event.newURL.indexOf('#');
