81e8fedf6c8b4069dc756e417b9f10498421d998
Iulian Ilie-Nemedi <ilie.nemedi.iulian@gmail.com>
2024-01-08 00:48:06 +0200

    INT - Updates

--- chat-redux/server/server.mjs
+++ chat-redux/server/server.mjs
@@ -1,9 +1,14 @@
+import express from 'express';
+import {join, resolve} from 'path';
 import { WebSocketServer } from 'ws';
+const application = express();
 const port = process.env.PORT || 8080;
-const server = new WebSocketServer({port});
-server.on('connection', socket =>
+const httpServer = application.listen(port, () => `Server ia running on port ${port}.`);
+const wsServer = new WebSocketServer({server: httpServer});
+application.use(express.static(join(resolve('..'), 'client/build')));
+wsServer.on('connection', socket =>
 	socket.on('message', data =>
-		server.clients.forEach(client =>
+		wsServer.clients.forEach(client =>
 			client.send(data)
 		)
 	)
