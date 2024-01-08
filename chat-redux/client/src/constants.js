5d99280c6ca0a8b83231d58f510f69bc73d6aa83
Iulian Ilie Nemedi <iilie@axway.com>
2024-01-08 12:28:02 +0200

    INT - Merge

--- chat-redux/client/src/constants.js
+++ chat-redux/client/src/constants.js
@@ -4,3 +4,4 @@ export const SEND = 'send';
 export const RECEIVE = 'recieve';
 export const CHAT = 'chat';
 export const ENDPOINT = `${window.location.protocol.replace('http', 'ws')}//${window.location.hostname}${window.location.port ? ':8080' : ''}`;
+
