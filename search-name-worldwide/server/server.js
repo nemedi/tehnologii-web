7292e6ae30756f3aa83c5e4d289bb0d7731f388d
Iulian ILIE-NEMEDI <iilie@axway.com>
2024-10-21 06:40:54 +0300

    INT - Cleanup

--- search-name-worldwide/server/server.js
+++ search-name-worldwide/server/server.js
@@ -3,7 +3,7 @@ const searchName = require('./service');
 const PORT = process.env.PORT || 8080;
 
 express()
-    .use(express.static('./client'))
+    .use(express.static('../client'))
     .get('/names/:name', async (request, response) =>
         response.json(await searchName(request.params.name)))
     .listen(PORT, () => console.log(`Server is listening on port ${PORT}.`));
