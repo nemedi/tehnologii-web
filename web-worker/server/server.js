19a8e63376b536523f89818655554cfef00827d2
Iulian ILIE-NEMEDI <iilie@axway.com>
2024-11-11 00:47:45 +0200

    INT - Web Worker

--- web-worker/server/server.js
+++ web-worker/server/server.js
@@ -1,6 +1,5 @@
 const express = require('express');
-const {join, resolve} = require('path');
 const PORT = process.env.PORT || 8080;
 express()
-	.use(express.static(join(resolve(), 'public')))
+	.use(express.static('../client'))
 	.listen(PORT, () => console.log(`Server is running on port ${PORT}.`));
