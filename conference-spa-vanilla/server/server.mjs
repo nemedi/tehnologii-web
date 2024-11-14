a42fa83e80dc3ea55b3f1ddfe8820a05f5aad92d
Iulian ILIE-NEMEDI <iilie@axway.com>
2024-11-14 23:45:19 +0200

    INT - Cleanup

--- conference-spa-vanilla/server/server.mjs
+++ conference-spa-vanilla/server/server.mjs
@@ -1,13 +1,12 @@
 import { config } from 'dotenv';
 import express, { json } from 'express';
-import { join, resolve } from 'path';
 import { initialize } from './repository.mjs';
 import router from './router.mjs';
 config();
 const PORT = process.env.PORT || 8080;
 express()
 	.use(json())
-	.use(express.static(join(resolve('..'), 'client')))
+	.use(express.static('../client'))
 	.use('/models', router)
 	.listen(PORT, async () => {
 		try {
