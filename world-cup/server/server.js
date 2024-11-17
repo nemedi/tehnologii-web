4a56b026c41470deb2caa8608569a9c06bd60e7d
Iulian ILIE-NEMEDI <iilie@axway.com>
2024-11-18 00:56:59 +0200

    INT - Refactoring

--- world-cup/server/server.js
+++ world-cup/server/server.js
@@ -1,9 +1,8 @@
 const express = require('express');
-const {join, resolve} = require('path');
 const {getBoard, getStandings, getMatches} = require('./service')('repository.json');
-const port = process.env.port || 8080;
+const PORT = process.env.PORT || 8080;
 express()
-	.use(express.static(join(resolve('..'), 'client')))
+	.use(express.static('../client'))
 	.get('/api/board', (request, response) => 
 		response.json(getBoard())
 	)
@@ -13,4 +12,4 @@ express()
 	.get('/api/matches/:team', (request, response) => 
 		response.json(getMatches(request.params.team))
 	)
-	.listen(port, () => console.log(`Server is running on port ${port}.`));
+	.listen(PORT, () => console.log(`Server is running on port ${PORT}.`));
