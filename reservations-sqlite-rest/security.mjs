5ae2c6ff8a452271d16047d0cc7b8023877cee75
Iulian ILIE-NEMEDI <iilie@axway.com>
2024-11-19 14:26:53 +0200

    INT - Updates

--- reservations-sqlite-rest/security.mjs
+++ reservations-sqlite-rest/security.mjs
@@ -1,10 +1,9 @@
-import {join, resolve} from 'path';
 import {existsSync, readFileSync} from 'fs';
 import basicAuth from 'express-basic-auth';
 
 export default basicAuth({
 	authorizer: (user, password) => {
-		const file = join(resolve(), 'credentials.json');
+		const file = './credentials.json';
 		console.log(`Authenticate against ${file}.`);
 		if (existsSync(file)) {
 			const credentials = JSON.parse(readFileSync(file));
