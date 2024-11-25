1d5cfc5c6ab92c1916350c192dad5fc7ed522ae4
Iulian ILIE-NEMEDI <iilie@axway.com>
2024-11-25 11:34:18 +0200

    INT - Cleanup

--- presidential-elections/client/index.js
+++ presidential-elections/client/index.js
@@ -1,8 +1,8 @@
 window.onload = async function() {
     document.getElementById('chart')
         .pieChart(async () => await (await fetch('/results')).json(), {
-            title: 'Rezultate alegeri prezidentiale 2024',
-            colors: ['#E7869D', '#EF4D54', '#6D9ECE', '#F5B661', '#FFE866', '#A6D6FE', '#6BC48D',
+            title: 'Rezultatele alegerilor prezidentiale 2024 turul 1',
+            colors: ['#E7869D', '#6D9ECE', '#EF4D54', '#F5B661', '#FFE866', '#A6D6FE', '#6BC48D',
                 '#F9A69B', '#7D8AB5', '#A998E3', '#6BA87A', '#6BA1C7', '#8A7EBF', '#E2EB9F']
         });
 };
