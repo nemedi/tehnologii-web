da4db15c23bc4d84beff89430d32e7736787359d
Iulian ILIE-NEMEDI <iilie@axway.com>
2025-01-12 22:20:43 +0200

    INT - Updates

--- talk-express-ws/server/server.js
+++ talk-express-ws/server/server.js
@@ -2,15 +2,16 @@ const express = require('express');
 const application = express();
 const wss = require('express-ws')(application).getWss();
 const messages = [];
-application.use(express.static(`${__dirname}/public`))
+const PORT = process.env.PORT || 8080;
+application.use(express.static('../client'))
 	.ws('/', (socket, request) => {
 		socket.send(JSON.stringify(messages));
 		socket.on('message', message => {
 			messages.push(message);
-			Array.from(wss.clients).forEach(client =>
-				client.send(JSON.stringify(message)));
+			Array.from(wss.clients)
+				.forEach(client =>
+					client.send(JSON.stringify(message)));
 		});
 	})
-	.listen(process.env.PORT || 8080,
-		() => console.log('Server is running.'));
+	.listen(PORT, () => console.log(`Server is running on port ${PORT}.`));
 
