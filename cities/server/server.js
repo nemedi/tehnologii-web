005666181ee31a0bb41b076f5468f32091503a15
Iulian ILIE-NEMEDI <iilie@axway.com>
2025-10-19 12:50:22 +0300

    INT - Cleanup

--- cities/server/server.js
+++ cities/server/server.js
@@ -18,7 +18,7 @@ express()
 					});
 				})
 				.read();
-			console.log(`Server is ruinning on port ${PORT}.`);
+			console.log(`Server is running on port ${PORT}.`);
 		} catch (error) {
 			console.error(error);
 		}
