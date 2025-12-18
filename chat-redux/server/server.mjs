b64f4e749784020b0d207de77ab4a48af860858f
Iulian ILIE-NEMEDI <iilie@axway.com>
2025-12-18 21:52:30 +0200

    INT - Updates

--- chat-redux/server/server.mjs
+++ chat-redux/server/server.mjs
@@ -1,7 +1,7 @@
 import express from 'express';
 import { WebSocketServer } from 'ws';
 const application = express();
-const port = process.env.PORT || 8080;
+const port = 8080;
 const httpServer = application.listen(port, () => `Server is running on port ${port}.`);
 const wsServer = new WebSocketServer({server: httpServer});
 application.use(express.static('../client/build'));
